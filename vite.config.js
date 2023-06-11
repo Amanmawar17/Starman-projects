36cfec7808f4915690adc79661ce67ba7210e4ea
Frieden <amanmawar17141@gmail.com>
2023-06-12 00:05:25 +0530

    fixing base in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://amanmawar17.github.io/frontend-mentor/',
+  base:'/frontend-mentor/',
   plugins: [react()],
 })
